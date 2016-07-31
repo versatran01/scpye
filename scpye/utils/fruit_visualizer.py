3eb0273d77e83e7359d7ed6c98d01ee9b340fadd
Chao Qu <quchao@seas.upenn.edu>
2016-07-30 20:09:52 -0400

    refactor some methods from DataManager to BagManager

--- scpye/utils/fruit_visualizer.py
+++ scpye/utils/fruit_visualizer.py
@@ -1,13 +1,11 @@
 from __future__ import (print_function, absolute_import, division)
 
-import os
-import cv2
-import numpy as np
 import matplotlib.pyplot as plt
+from scpye.improc.image_processing import u8_from_bw
 
 
 class FruitVisualizer(object):
-    def __init__(self, image_dir, bag_ind, save_image=False):
+    def __init__(self, pause_time=0.01):
         self.fig = plt.figure()
         plt.ion()
         self.ax_bgr = self.fig.add_subplot(121)
@@ -15,19 +13,9 @@ class FruitVisualizer(object):
         self.h_bgr = None
         self.h_bw = None
 
-        self.pause_time = 0.001
-        self.i = 0
-
-        self.image_dir = os.path.join(image_dir, 'frame' + str(bag_ind))
-        self.bag_ind = bag_ind
-        self.save_image = save_image
-
-        self.bgr_name = 'bgr{0:04d}.png'
-        self.bw_name = 'bw{0:04d}.png'
+        self.pause_time = pause_time
 
     def show(self, disp_bgr, disp_bw):
-        if disp_bw.dtype == bool:
-            disp_bw = np.array(disp_bw, dtype='uint8') * 255
 
         if self.h_bgr is None:
             self.h_bgr = self.ax_bgr.imshow(disp_bgr)
@@ -35,15 +23,5 @@ class FruitVisualizer(object):
         else:
             self.h_bgr.set_data(disp_bgr)
             self.h_bw.set_data(disp_bw)
-
-        self.i += 1
-
-        if self.save_image:
-            bgr_name = os.path.join(self.image_dir,
-                                    self.bgr_name.format(self.i))
-            bw_name = os.path.join(self.image_dir,
-                                   self.bw_name.format(self.i))
-            cv2.imwrite(bgr_name, disp_bgr)
-            cv2.imwrite(bw_name, disp_bw)
 
         plt.pause(self.pause_time)
--- scripts/split_blobs.py
+++ scripts/split_blobs.py
@@ -1,57 +1,32 @@
-import os
 import cv2
-import matplotlib.pyplot as plt
 from scpye.improc.binary_cleaner import BinaryCleaner
 from scpye.improc.blob_analyzer import BlobAnalyzer
-from scpye.utils.data_manager import DataManager
-from scpye.utils.drawing import imshow, draw_bboxes
 from scpye.improc.image_processing import enhance_contrast
+from scpye.utils.data_manager import DataManager
+from scpye.utils.bag_manager import BagManager
+from scpye.utils.drawing import draw_bboxes
+from scpye.utils.fruit_visualizer import FruitVisualizer
 
-# %%
 base_dir = '/home/chao/Workspace/dataset/agriculture'
 color = 'red'
 mode = 'slow_flash'
 side = 'north'
-bag_ind = 2
+bag_ind = 1
 
-# %%
 dm = DataManager(base_dir, color=color, mode=mode, side=side)
-image_dir = os.path.join(dm.image_dir, "frame" + str(bag_ind))
+bm = BagManager(dm.data_dir, bag_ind)
+bc = BinaryCleaner(ksize=3, iters=2, min_area=5)
+ba = BlobAnalyzer(max_cntr_area=100)
+fv = FruitVisualizer(pause_time=0.1)
 
-bw_name = 'bw{0:04d}.png'
-bgr_name = 'bgr{0:04d}.png'
-
-plt.ion()
-fig = plt.figure(figsize=(12, 16))
-ax1 = fig.add_subplot(121)
-ax2 = fig.add_subplot(122)
-h1, h2 = None, None
-
-for i in range(20, 50):
-    bw_file = os.path.join(image_dir, bw_name.format(i))
-    bgr_file = os.path.join(image_dir, bgr_name.format(i))
-    bw = cv2.imread(bw_file, cv2.IMREAD_GRAYSCALE)
-    bgr = cv2.imread(bgr_file, cv2.IMREAD_COLOR)
-
-    # %%
-    bc = BinaryCleaner(ksize=3, iters=2, min_area=4)
-    bw, region_props = bc.clean(bw)
-
-    disp_bgr = enhance_contrast(bgr)
-    disp_bw = cv2.cvtColor(bw, cv2.COLOR_GRAY2BGR)
-
-    # %%
-    ba = BlobAnalyzer()
+for bgr, bw in bm.load_detect():
+    bw_clean, region_props = bc.clean(bw)
     fruits = ba.analyze(bgr, region_props)
 
-    draw_bboxes(disp_bgr, fruits, color=(0, 255, 0))
-    draw_bboxes(disp_bw, fruits, color=(0, 255, 0))
-    if h1 is None:
-        h1 = ax1.imshow(disp_bgr, interpolation='none')
-        h2 = ax2.imshow(disp_bw, interpolation='none')
-    else:
-        h1.set_data(disp_bgr)
-        h2.set_data(disp_bw)
-    plt.pause(0.1)
-    plt.draw()
+    disp_bgr = enhance_contrast(bgr)
+    disp_bw = cv2.cvtColor(bw_clean, cv2.COLOR_GRAY2BGR)
 
+    draw_bboxes(disp_bgr, fruits)
+    draw_bboxes(disp_bw, fruits)
+
+    fv.show(disp_bgr, disp_bw)
