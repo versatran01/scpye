d71168ebe1f1d1fdda0f23066147165fdfeb8657
Chao Qu <quchao@seas.upenn.edu>
2016-10-22 19:59:18 -0400

    add script to extract image

--- scripts/rectify_bag.py
+++ scripts/rectify_bag.py
@@ -4,7 +4,7 @@ import os
 from tqdm import tqdm
 import rosbag
 import numpy as np
-from scipy.misc import imsave
+import cv2
 from image_geometry.cameramodels import PinholeCameraModel
 from cv_bridge import CvBridge
 
@@ -29,6 +29,7 @@ cinfo_topic = camera_topic + '/camera_info'
 bridge = CvBridge()
 cam_model = PinholeCameraModel()
 i = 0
+s = 10
 with rosbag.Bag(bag_file) as bag:
     for topic, msg, t in tqdm(bag.read_messages()):
         # first initialize camera model
@@ -38,14 +39,13 @@ with rosbag.Bag(bag_file) as bag:
         
         # after camera model initialized, read image
         if cam_model.K is not None and topic == image_topic:
+            i += 1
+            if i % s != 0:
+                continue
             image = bridge.imgmsg_to_cv2(msg, 'bgr8')
             image_rect = np.empty_like(image)
             cam_model.rectifyImage(image, image_rect)
             image_name = 'rect_{0:04}.png'.format(i)
             image_file = os.path.join(image_dir, image_name)
-            imsave(image_file, image)
-            i += 1
+            cv2.imwrite(image_file, image_rect)
             
-            
-        
-    
